2db8eeb41d5b4093138a8450d550071c89f6daaa
Alexander Sauro <alesauro30@gmail.com>
2026-07-26 22:25:11 -0300

    feat: add AUTH_SECRET logging and set secret in NextAuth configuration

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -4,9 +4,10 @@ import type { User } from "@/lib/types/auth";
 
 const API_BASE_URL =
   process.env.NEXT_PUBLIC_API_URL || "https://app-api.sabturno.com";
-
+console.log("AUTH_SECRET disponible:", Boolean(process.env.AUTH_SECRET));
 export const { handlers, signIn, signOut, auth } = NextAuth({
   trustHost: true,
+  secret: process.env.AUTH_SECRET,
   providers: [
     Credentials({
       name: "credentials",
